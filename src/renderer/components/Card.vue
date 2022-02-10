4310d91b459473b35fd0455f96315a7c54207604
Ben Huang <benxu.huang@gmail.com>
2022-02-10 16:04:24 +0800

    feat: add book image on the ShowKoboStat page

--- src/renderer/components/Card.vue
+++ src/renderer/components/Card.vue
@@ -1,6 +1,9 @@
 <template>
   <Col span="8" offset="2" style="margin-bottom: 15px">
     <Card>
+      <a @click="openUrl(book.ISBN)">
+        <img v-bind:src="bookImg" style="max-width: 120px; max-height: 170px" />
+      </a>
       <p slot="title" class="book-title-link" @click="openUrl(book.ISBN)">
         {{ book.Title }}
       </p>
@@ -21,12 +24,19 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   props: {
     book: {
       type: Object,
       required: true,
     },
+  },
+  data() {
+    return {
+      bookImg: "",
+    };
   },
   methods: {
     millisecondToHours(millisecond) {
@@ -36,6 +46,30 @@ export default {
       const url = `https://www.kobo.com/tw/zh/search?query=${isbn}`;
       this.$electron.shell.openExternal(url);
     },
+    getBookImage(title, queryAuthor, isbn) {
+      const googleApi = "https://www.googleapis.com/books/v1/volumes";
+      const url = `${googleApi}?q=${title}+${queryAuthor}+${isbn}&filter=paid-ebooks&orderBy=newest&maxResults=1`;
+      axios
+        .get(url)
+        .then((response) => {
+          if (response.data.items == undefined) {
+            this.bookImg =
+              "https://via.placeholder.com/120x170?text=no%20image";
+          } else {
+            console.log(title);
+            console.log(queryAuthor);
+            console.log(url);
+            this.bookImg =
+              response.data.items[0].volumeInfo.imageLinks.smallThumbnail;
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+  created() {
+    this.getBookImage(this.book.Title, this.book.Attribution, this.book.ISBN);
   },
 };
 </script>
